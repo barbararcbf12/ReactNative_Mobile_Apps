1bdef5de67f5fa03b0843646148e05ad3e8b3abc
*Babi <barbara@onlaw.dk>
2018-04-04 18:18:24 +0200

    Update authentication.js

--- Tryg_Invest_App/src/components/authentication.js
+++ Tryg_Invest_App/src/components/authentication.js
@@ -2,11 +2,11 @@ import * as firebase from 'firebase';
 
 const config = {
 	
-	apiKey: "AIzaSyBd6qjW9QG4cU0kQEiEgZHq61ikVzp21yI",
-	authDomain: "udemycourse-6c3c7.firebaseapp.com",
-	databaseURL: "https://udemycourse-6c3c7.firebaseio.com",
-	storageBucket: "udemycourse-6c3c7.appspot.com",
-	messagingSenderId: "1027316711290"
+	apiKey: "",
+	authDomain: "XXX.firebaseapp.com",
+	databaseURL: "https://XXX.firebaseio.com",
+	storageBucket: "XXX.appspot.com",
+	messagingSenderId: "XXX"
 
   };
 
